f54a3f1e796ed96c769954159c291d983fcf5fd5
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-11-01 13:07:43 +0200

    Add promiseAll, race, allSettled example

--- promise.js
+++ promise.js
@@ -4,10 +4,21 @@ const p1 = new Promise((resolve, reject) => {
     } else {
         reject("rejected");
     }
-});
-
-
-p1
+})
     .then(value => `${value} hello`)
     .then(value => console.log("this is the value: " + value))
     .catch(console.log);
+
+const p2 = new Promise(resolve => resolve({name: "Ruslan"}))
+    .then(res => console.log(res?.name))
+
+
+Promise.all([p1, p2]);
+
+const getAwaited = async () => await Promise.race([p1]);
+
+console.log(getAwaited());
+
+let promise = Promise.allSettled([p1, p2]);
+
+console.log("p2",  promise);
