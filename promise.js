4713d2b037c98b80027596f98f81b50b930d37a3
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-11-01 12:37:43 +0200

    Add promise example

--- promise.js
+++ promise.js
@@ -1,1 +1,13 @@
-console.log("hello, promise");
+const p1 = new Promise((resolve, reject) => {
+    if (true) {
+        resolve("resolved");
+    } else {
+        reject("rejected");
+    }
+});
+
+
+p1
+    .then(value => `${value} hello`)
+    .then(value => console.log("this is the value: " + value))
+    .catch(console.log);
